48fd237ef38f0096d3e6b821165943e519671378
Douglas Moura <douglas.ademoura@gmail.com>
2021-06-09 10:21:27 -0300

    chore: add app's global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import '../styles/global.scss';
+import styles from '../styles/pages/app.module.scss';
 
-export default MyApp;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.container}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
